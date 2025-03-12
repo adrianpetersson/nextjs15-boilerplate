42cc3657d4fbe78e5edfeed1c9121f30017bcaf9
Adrian <adriantimothypetersson@gmail.com>
2025-03-12 22:29:34 +0100

    feat: update lint-staged configuration and restore typedEnv in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    typedEnv: true,
     turbo: {
       rules: {
         "*.svg": {
-          loaders: ["@svgr/webpack"],
           as: "*.js",
+          loaders: ["@svgr/webpack"],
         },
       },
     },
+    typedEnv: true,
   },
 };
 
