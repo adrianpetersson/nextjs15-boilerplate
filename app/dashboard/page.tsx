dc5078962bb98bec899c019d9566fb0a84fd9b6c
Adrian <adriantimothypetersson@gmail.com>
2025-03-14 19:11:35 +0100

    feat: enhance Dashboard with user authentication and add Not Found page; remove Initializing component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,11 @@
-export default function Dashboard() {
+import { auth } from "@clerk/nextjs/server";
+
+export default async function Dashboard() {
+  const { userId } = await auth();
   return (
     <div className="min-h-screen items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="flex flex-col gap-8 sm:items-start">
-        <p>Protected route example</p>
+        <p>Protected route example {userId}</p>
       </main>
     </div>
   );
