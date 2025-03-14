e75f109956b3e13a47bfc0b09767d05f03863174
Adrian <adriantimothypetersson@gmail.com>
2025-03-14 23:21:29 +0100

    feat: enhance SVG handling in webpack configuration

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -11,6 +12,35 @@ const nextConfig: NextConfig = {
       },
     },
     typedEnv: true,
+  },
+
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    // @ts-expect-error - We know this rule exists
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule?.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        resourceQuery: /url/, // *.svg?url
+        test: /\.svg$/i,
+      },
+      // Convert all other *.svg imports to React components
+      {
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        test: /\.svg$/i,
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
   },
 };
 
