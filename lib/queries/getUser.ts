c7990074907208dbfc53b49b4ecbc7405e2fdbe7
Adrian <adriantimothypetersson@gmail.com>
2025-03-16 09:48:58 +0100

    feat: update getUser function to authenticate user and fetch by clerkId

--- lib/queries/getUser.ts
+++ lib/queries/getUser.ts
@@ -1,10 +1,17 @@
+import { auth } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
 
 import { db } from "@/db";
 import { users } from "@/db/schema";
 
-export const getUser = async (id: number) => {
-  const user = await db.select().from(users).where(eq(users.id, id));
+export const getUser = async () => {
+  const { userId } = await auth();
+
+  if (!userId) throw new Error("User not found");
+
+  const user = await db.select().from(users).where(eq(users.clerkId, userId));
+
+  if (!user[0]) throw new Error("User not found");
 
   return user[0];
 };
